ebb9fe190ab0b9e9a9b8ad2ad1c01f808e9d68ba
tsunemori-akane <po6271933@outlook.com>
2025-11-09 20:47:00 +0800

    05: authentication socials

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,17 @@
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { auth } from "@/lib/auth";
 import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
 
-const Page = () => {
+const Page = async () => {
   console.log("sign in page");
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!!session) {
+    redirect("/");
+  }
   return <SignInView></SignInView>;
 };
 
